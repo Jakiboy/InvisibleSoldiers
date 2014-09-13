42616b8222755d992d632be5499430af61997113
JIHAD SINNAOUR <j.sinnaour.official@gmail.com>
2014-09-13 03:29:40 +0100

    Create index.php

---
+++ index.php
@@ -0,0 +1,71 @@
+<?php
+/*
+@autor   : JIHAD SINNAOUR
+@Company : FuturComp 2014
+*/
+$file = './Export/fb.xml';
+$sourcePath = "./Import/index.html" ;
+if(!file_exists($sourcePath)){
+	echo 'No Source file found !';
+	exit;
+}
+else{
+	$source = file_get_contents($sourcePath);
+file_put_contents($file, $source);
+//header('Content-Type: text/plain');
+// FIRST STEP 1
+$str = $source ;
+$start = '{"list":[';
+$end = '"]}';
+
+$pattern = sprintf(
+    '/%s(.+?)%s/ims',
+    preg_quote($start, '/'), preg_quote($end, '/')
+);
+if (preg_match($pattern, $str, $matches)) {
+    list(, $match) = $matches;
+    //echo '<p>your invisible Soldier is : </p>';
+    //echo '<a href="https://www.facebook.com/'.$match.'">Click too see who ?</a>'. "<br>";
+}
+// SECEND STEP 2
+$str = $match ;
+
+$start = '"';
+$end = '-0"';
+
+$pattern = sprintf(
+    '/%s(.+?)%s/ims',
+    preg_quote($start, '/'), preg_quote($end, '/')
+);
+if (preg_match($pattern, $str, $matches)) {
+    list(, $match) = $matches;
+}
+// THIRD STEP 3
+// Start the buffering //
+$text_line = $match;
+$text_line = explode('-2",',$text_line);
+for ($start=0; $start < count($text_line); $start++) {
+$match = $text_line[$start];
+echo $match ;
+
+}
+echo '<br>';
+// FIRTH STEP 4
+if(!file_exists('./Export/clean.txt')){
+	echo 'No Source file found !'.'<br>';
+	exit;
+}
+else{
+$match = file_get_contents('./Export/clean.txt');
+$text_line = $match;
+$text_line = explode('"',$text_line);
+
+//for ($start=0; $start < count($text_line); $start++) {
+for ($start=0; $start < 10; $start++) {
+
+$match = $text_line[$start];
+echo '<a href="https://www.facebook.com/'.$match.'">Click too see who ?</a>'. "<br>";
+}
+}
+}
+?>
